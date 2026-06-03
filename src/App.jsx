fd2603cb7c8b7ed0d93aa1344a5c1820fbe060a7
Praveen Bajpai <bajpaipraveen0@gmail.com>
2026-06-03 16:33:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import AppRoutes from './routes/AppRoutes'
+import Footer from './components/Footer.jsx'
+import Navbar from './components/Navbar.jsx'
+import AppRoutes from './routes/AppRoutes.jsx'
 
 function App() {
 
